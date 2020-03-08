710f09c89583926e3bc2004fdcdbf09df847f34c
Moe Alhassan <maalha@amazon.com>
2020-03-08 17:30:24 -0500

    styling changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import React from "react";
 const Hero = () => (
   <div className="text-center hero my-5">
    {/* <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" /> */}
-    <h2 className="mb-5">Jeeves makes renting seamless.</h2>
+    <h2 className="mb-5">Jeeves makes renting <span className="italic">seamless</span>.</h2>
     </div>
 );
 
